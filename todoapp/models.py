cfb370a9812f33ff37e44c2045e19f8a6df7192f
kinuthiajr <kinuthiajr@gmail.com>
2024-04-16 14:35:00 +0300

    List api

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Todo(models.Model):
+    task = models.CharField(max_length=180)
+    timestamp = models.DateTimeField(blank=True,auto_now=False,auto_now_add=True)
+    comepleted = models.BooleanField(blank=True,default=False)
+    updated = models.DateTimeField(blank=True,auto_now=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,blank=True,null=False)
+
+    def __str__(self):
+        return self.task
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
+from .models import Todo
+from .serializers import TodoSerializer
 
-# Create your views here.
+class TodoListApiView(APIView):
+    # add permissions to check if user is authenticated
+    permission_classes = [permissions.IsAuthenticated]
+
+    # 1 list all
+    def get(self,request,*args,**kwargs):
+        """
+        list all tasks by a user
+        """
+        todos = Todo.objects.filter(user=request.user.id)
+        serializer = TodoSerializer(todos,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    
+    # 2 Create a Todo by a user
+    def post(self,request,*args,**kwargs):
+        """"
+        Create a todo with a given data by a user
+        """
+        data = {
+            'task':request.data.get('task'),
+            'completed':request.data.get('completed'),
+            'user':request.user.id
+        }
+        serializer = TodoSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_404_BAD_REQUEST)
+    
