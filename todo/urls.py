e1c78aa2507eaa5c2d5b8df67c2db92372b5fc80
kinuthiajr <kinuthiajr@gmail.com>
2024-04-16 15:31:29 +0300

    ListView API endpoints

--- todo/urls.py
+++ todo/urls.py
@@ -17,8 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    
+    path('todos/', include('todoapp.urls')),
 ]
